ca0974acc81fce3323f215faad78fe9243014cfe
ahmed00medhat <ahmed@egycart.com>
2015-08-15 19:03:55 +0200

    Original opencart version 1.5.4

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev.egycart.com/admin/');
+define('HTTP_CATALOG', 'http://dev.egycart.com/');
+define('HTTP_IMAGE', 'http://dev.egycart.com/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev.egycart.com/admin/');
+define('HTTPS_CATALOG', 'http://dev.egycart.com/');
+define('HTTPS_IMAGE', 'http://dev.egycart.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/nginx/html/devegycart/admin/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/devegycart/system/');
+define('DIR_DATABASE', '/usr/share/nginx/html/devegycart/system/database/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/devegycart/admin/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/devegycart/admin/view/template/');
+define('DIR_CONFIG', '/usr/share/nginx/html/devegycart/system/config/');
+define('DIR_IMAGE', '/usr/share/nginx/html/devegycart/image/');
+define('DIR_CACHE', '/usr/share/nginx/html/devegycart/system/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/devegycart/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/devegycart/system/logs/');
+define('DIR_CATALOG', '/usr/share/nginx/html/devegycart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'devopencart');
+define('DB_PASSWORD', 'devopencart');
+define('DB_DATABASE', 'devopencart');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev.egycart.com/');
+define('HTTP_IMAGE', 'http://dev.egycart.com/image/');
+define('HTTP_ADMIN', 'http://dev.egycart.com/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev.egycart.com/');
+define('HTTPS_IMAGE', 'http://dev.egycart.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/nginx/html/devegycart/catalog/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/devegycart/system/');
+define('DIR_DATABASE', '/usr/share/nginx/html/devegycart/system/database/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/devegycart/catalog/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/devegycart/catalog/view/theme/');
+define('DIR_CONFIG', '/usr/share/nginx/html/devegycart/system/config/');
+define('DIR_IMAGE', '/usr/share/nginx/html/devegycart/image/');
+define('DIR_CACHE', '/usr/share/nginx/html/devegycart/system/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/devegycart/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/devegycart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'devopencart');
+define('DB_PASSWORD', 'devopencart');
+define('DB_DATABASE', 'devopencart');
+define('DB_PREFIX', '');
+?>
